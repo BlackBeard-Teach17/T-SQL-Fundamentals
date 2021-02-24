bd8dbf2b484fda0394bff1141f62c720834692c8
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-02-24 07:26:34 +0200

    Add files via upload

--- Auction.sql
+++ Auction.sql
@@ -1,32 +1,13 @@
 CREATE DATABASE Auction;
 
+--DROP DATABASE Auction;
+
 BACKUP DATABASE Auction
     TO DISK = 'C:\backups\auctionDB.bak'
 WITH DIFFERENTIAL;
 
 USE Auction;
 GO
-
-CREATE TABLE Users
-(
-    UserID INT IDENTITY (1,1),
-    FirstName VARCHAR(200) NOT NULL,
-    LastName VARCHAR(200) NOT NULL,
-    AddressLine1 VARCHAR(200) NOT NULL,
-    AddressLine2 VARCHAR(200),
-    CITY VARCHAR(200) NOT NULL,
-    PostalCode VARCHAR(10) NOT NULL,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    PASSWORD VARCHAR(1000) NOT NULL,
-    IdentityNo INT NOT NULL UNIQUE,
-    Status VARCHAR(20) REFERENCES UserStatus(StatusType) DEFAULT 'Unverified',
-    TMStamp TIMESTAMP
-
-    CONSTRAINT PK_USER_ID PRIMARY KEY CLUSTERED
-    (
-        UserID
-    )
-);
 
 CREATE TABLE Officiators
 (
@@ -44,17 +25,40 @@ CREATE TABLE UserStatus
     StatusType VARCHAR(50),
     TMStamp TIMESTAMP,
     CONSTRAINT PK_Status PRIMARY KEY (StatusID)
-)
+);
+
+
+CREATE TABLE Users
+(
+    UserID INT IDENTITY (1,1),
+    FirstName VARCHAR(200) NOT NULL,
+    LastName VARCHAR(200) NOT NULL,
+    AddressLine1 VARCHAR(200) NOT NULL,
+    AddressLine2 VARCHAR(200),
+    CITY VARCHAR(200) NOT NULL,
+    PostalCode VARCHAR(10) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    PASSWORD VARCHAR(1000) NOT NULL,
+    IdentityNo INT NOT NULL UNIQUE,
+    Status VARCHAR(20) DEFAULT 'Unverified',
+    TMStamp TIMESTAMP
+
+    CONSTRAINT PK_USER_ID PRIMARY KEY CLUSTERED
+    (
+        UserID
+    )
+);
 
 CREATE TABLE Blacklist
 (
     BlacklistID INT IDENTITY (1,1),
     UserID INT FOREIGN KEY REFERENCES Users(UserID),
     FromBuying CHAR(1) NOT NULL DEFAULT 'N',
-    FromSelling CHAR(1) NOT NULL DEFAULT 'Y',
+    FromSelling CHAR(1) NOT NULL DEFAULT 'N',
     Reason TEXT NOT NULL,
-    TMStamp TIMESTAMP
-)
+    TMStamp TIMESTAMP, 
+	CONSTRAINT PK_BlacklistID PRIMARY KEY CLUSTERED(BlacklistID)
+);
 
 CREATE TABLE LivestockMark
 (
@@ -71,13 +75,14 @@ CREATE TABLE Category
     CategoryID INT IDENTITY(1,1),
     CategoryName VARCHAR(200) NOT NULL UNIQUE,
     TMStamp TIMESTAMP
+	CONSTRAINT PK_Category PRIMARY KEY CLUSTERED(CategoryID)
 );
 
 CREATE TABLE Breed
 (
     BreedID INT IDENTITY(1,1) UNIQUE,
     Breed VARCHAR(200) NOT NULL UNIQUE,
-    Category VARCHAR(200),
+    Category VARCHAR(200) REFERENCES Category(CategoryName),
     TMStamp TIMESTAMP,
     CONSTRAINT PK_Breed PRIMARY KEY (Breed)
 );
@@ -87,6 +92,7 @@ CREATE TABLE AuctionEvent
     AuctionEventID INT IDENTITY (1,1),
     OfficiatorID INT FOREIGN KEY REFERENCES Officiators(OfficiatorID),
     AuctionAddress VARCHAR(400) NOT NULL,
+	AuctionName VARCHAR(200) NOT NULL,
     AuctionDate DATE,
     NoOfSeats INT NOT NULL DEFAULT 50,
     AuctionType VARCHAR(30) NOT NULL DEFAULT 'Hybrid',
@@ -99,12 +105,12 @@ CREATE TABLE Bid
 (
     BidID INT IDENTITY (1,1),
     AuctionID INT FOREIGN KEY REFERENCES AuctionEvent(AuctionEventID),
-    ProductID INT FOREIGN KEY REFERENCES Product(ProductID),
     CurrentBid INT NOT NULL DEFAULT 0,
     WinBid INT NOT NULL DEFAULT '0',
     BuyerID INT FOREIGN KEY REFERENCES Users(UserID),
     CONSTRAINT PK_Bid PRIMARY KEY CLUSTERED (BidID)
 );
+
 
 CREATE TABLE Product
 (
@@ -115,11 +121,22 @@ CREATE TABLE Product
     Gender VARCHAR(7) DEFAULT 'Unknown',
     Lot INT,
     StartingBid INT NOT NULL DEFAULT 1000,
+	ProductStatus VARCHAR(20) NOT NULL DEFAULT 'Available',
     BidID INT FOREIGN KEY REFERENCES Bid(BidID),
     SellerID INT FOREIGN KEY REFERENCES Users(UserID),
     ProductImage VARBINARY(MAX)
-   -- WinningBid INT FOREIGN KEY REFERENCES
 );
+
+ALTER TABLE Product
+	ADD CONSTRAINT PK_ProductID
+	PRIMARY KEY(ProductID);
+
+ALTER TABLE Bid
+	ADD ProductID INT;
+
+ALTER TABLE Bid
+	ADD CONSTRAINT FK_ProductID
+	FOREIGN KEY (ProductID) REFERENCES Product(ProductID);
 
 CREATE TABLE Participant
 (
@@ -132,24 +149,41 @@ CREATE TABLE Participant
 CREATE TABLE Trans
 (
     TransID INT IDENTITY (1,1),
-    BuyerID INT FOREIGN KEY REFERENCES Bid(BuyerID),
+    BuyerID INT, --FOREIGN KEY REFERENCES Bid(BuyerID),Can't do this 
     --SellerID INT FOREIGN KEY REFERENCES
-    ProductID INT FOREIGN KEY REFERENCES Bid(ProductID),
+    ProductID INT, --FOREIGN KEY REFERENCES Bid(ProductID),
     VAT INT,
 );
 
 --INSERT STATEMENTS
-INSERT INTO UserStatus(StatusType, TMStamp) VALUES ('Unverified', GETDATE());
-INSERT INTO UserStatus(StatusType, TMStamp) VALUES ('Verified', GETDATE());
-INSERT INTO UserStatus(StatusType, TMStamp) VALUES ('Blacklisted', GETDATE());
+IF NOT EXISTS(SELECT StatusType FROM UserStatus WHERE StatusType = 'Unverified')
+	INSERT INTO UserStatus(StatusType) VALUES ('Unverified');
 
+IF NOT EXISTS(SELECT StatusType FROM UserStatus WHERE StatusType = 'Verified')
+INSERT INTO UserStatus(StatusType) VALUES ('Verified');
 
+IF NOT EXISTS(SELECT StatusType FROM UserStatus WHERE StatusType = 'Blacklisted')
+INSERT INTO UserStatus(StatusType) VALUES ('Blacklisted');
+
+--Category Inserts
+IF NOT EXISTS(SELECT CategoryName FROM Category WHERE CategoryName = 'Cow')
+	INSERT INTO Category(CategoryName) VALUES('Cow');
+IF NOT EXISTS(SELECT CategoryName FROM Category WHERE CategoryName = 'Pig')
+	INSERT INTO Category(CategoryName) VALUES('Pig');
+IF NOT EXISTS(SELECT CategoryName FROM Category WHERE CategoryName = 'Horse')
+	INSERT INTO Category(CategoryName) VALUES('Horse');
+IF NOT EXISTS(SELECT CategoryName FROM Category WHERE CategoryName = 'Goat')
+	INSERT INTO Category(CategoryName) VALUES('Goat');
+IF NOT EXISTS(SELECT CategoryName FROM Category WHERE CategoryName = 'Sheep')
+	INSERT INTO Category(CategoryName) VALUES('Sheep');
+IF NOT EXISTS(SELECT CategoryName FROM Category WHERE CategoryName = 'Donkey')
+	INSERT INTO Category(CategoryName) VALUES('Donkey');
 
 --Stored Procedure
 USE Auction;
 GO
 
-CREATE PROCEDURE dbo.spInsertUserDetails
+CREATE PROCEDURE uspInsertUserDetails
     @FirstName VARCHAR(200),
     @LastName VARCHAR(200),
     @AddressLine1 VARCHAR(200),
@@ -160,6 +194,7 @@ CREATE PROCEDURE dbo.spInsertUserDetails
     @Password VARCHAR(1000),
     @IdentityNo INT
 AS
+SET NOCOUNT ON
     BEGIN
         BEGIN TRY
             BEGIN TRANSACTION
@@ -177,14 +212,19 @@ AS
             ROLLBACK TRANSACTION;
         END CATCH
     END
-;
-CREATE PROCEDURE dbo.spUpdateUserAddressDetails
-    @UserID INT,
-    @AddressLine1 VARCHAR(200),
-    @AddressLine2 VARCHAR(200),
-    @CITY VARCHAR(200),
-    @PostalCode VARCHAR(10)
+GO;
+
+EXECUTE uspInsertUserDetails 'Zeke', 'Jaegar', '17 Beast Titan Street', 'Riberio', 'Marley', '20010', 'zeke@aot.com', '98a0276b3e0ea0ac9fffd81bd8fc602b', 0101015142084
+GO;
+
+CREATE PROCEDURE uspUpdateUserAddressDetails 
+		@UserID AS INT,
+		@AddressLine1 AS VARCHAR(200),
+		@AddressLine2 AS VARCHAR(200),
+		@CITY AS VARCHAR(200),
+		@PostalCode AS VARCHAR(10)
 AS
+SET NOCOUNT ON
     BEGIN
         BEGIN TRY
             BEGIN TRANSACTION
@@ -203,17 +243,63 @@ AS
             ROLLBACK TRANSACTION;
         END CATCH
     END
-;
-CREATE PROCEDURE dbo.spUserBids
+
+GO;
+
+CREATE PROCEDURE uspUserBids
+	@AuctionID INT,
     @CurrentBid INT,
-    @UserID INT
+    @BuyerID INT,
+	@ProductID INT
 AS
+SET NOCOUNT ON;
     BEGIN
         BEGIN TRY
             BEGIN TRANSACTION
-
+				IF @CurrentBid > ufn_LargestBid(@AuctionID, @ProductID)
+					INSERT INTO Bid (AuctionID, CurrentBid, BuyerID, ProductID)
+						VALUES(@AuctionID, @CurrentBid, @BuyerID, @ProductID)
+				ELSE
+					PRINT 'Place a higher bid'
             COMMIT TRANSACTION
         END TRY
         BEGIN CATCH
+			IF @@ERROR > 0
+				ROLLBACK TRANSACTION
+			PRINT ERROR_MESSAGE();
         END CATCH
     END
+GO;
+
+EXECUTE uspUserBids 1, 2000, 1, 2;
+GO
+
+
+--UDF to get the current largest bid
+CREATE FUNCTION dbo.ufn_LargestBid(@AuctionID INT, @ProductID INT)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @MaxBid INT;
+	SELECT @MaxBid = MAX(b.CurrentBid)
+	FROM Bid b
+	WHERE b.AuctionID = @AuctionID
+		AND b.ProductID = @ProductID;
+
+	IF(@MaxBid IS NULL)
+		SET @MaxBid = 0;
+	RETURN @MaxBid;
+END
+GO;
+--A trigger to update the Products table to set ProductStatus to sold after successful bidding
+CREATE TRIGGER registration_reminder
+ON Users
+AFTER INSERT
+AS
+EXEC msdb.dbo.sp_send_dbmail
+	@profile_name = 'Auction Admin',
+	@recipients = 
+	
+	DECLARE @LastName AS VARCHAR(200) = (SELECT LastName FROM Users WHERE UserID = MAX(UserID));
+
+	@profile_name = 
